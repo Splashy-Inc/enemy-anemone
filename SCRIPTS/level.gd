bd4acba7c0f0be239660e1e2519203e45e99c5eb
avelitj12 <avelitj12@gmail.com>
2025-04-19 16:33:00 -0400

    Win/lose conditions

--- SCRIPTS/level.gd
+++ SCRIPTS/level.gd
@@ -2,10 +2,16 @@ extends Node
 
 class_name Level
 
+signal won
+signal lost
+
 func _ready() -> void:
 	for child in get_children():
 		if child is DialogBox:
 			child.dialog_ended.connect(_on_dialog_ended.bind(child))
+	
+	for objective in get_tree().get_nodes_in_group("Objective"):
+		objective.died.connect(_on_objective_died)
 
 func _process(delta: float) -> void:
 	pass
@@ -15,3 +21,12 @@ func _physics_process(delta: float) -> void:
 
 func _on_dialog_ended(dialog_box: DialogBox):
 	print_debug("Dialog ended: ", dialog_box)
+
+func resume_play(mouse_mode: int = Input.MOUSE_MODE_VISIBLE):
+	process_mode = PROCESS_MODE_INHERIT
+
+func pause_play(mouse_mode: int = Input.MOUSE_MODE_VISIBLE):
+	process_mode = PROCESS_MODE_DISABLED
+
+func _on_objective_died():
+	lost.emit()
