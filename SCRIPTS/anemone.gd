04e99278bb8281462b1227cb9b4fe8148c8003c9
avelitj12 <avelitj12@gmail.com>
2025-04-19 15:37:32 -0400

    Anemone deal damage to snail

--- SCRIPTS/anemone.gd
+++ SCRIPTS/anemone.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var speed := 50
 @export var target: Node2D
 
+@onready var attack_timer: Timer = $AttackRadius/AttackTimer
+
 var in_range_targets := []
 
 func _physics_process(delta: float) -> void:
@@ -11,7 +13,10 @@ func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(target.global_position)
 	if direction and not target in in_range_targets:
 		velocity = direction.normalized() * speed
+		attack_timer.stop()
 	else:
+		if attack_timer.is_stopped():
+			attack_timer.start()
 		velocity = Vector2.ZERO
 
 	move_and_slide()
@@ -19,6 +24,14 @@ func _physics_process(delta: float) -> void:
 func _on_attack_radius_body_entered(body: Node2D) -> void:
 	if not body in in_range_targets:
 		in_range_targets.append(body)
+		if body != target:
+			target = body
 
 func _on_attack_radius_body_exited(body: Node2D) -> void:
 	in_range_targets.erase(body)
+	if body == target:
+		target = null
+
+func _on_attack_timer_timeout() -> void:
+	if target:
+		target.on_hit(1)
