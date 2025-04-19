96203446492a6c045b5f9c10db548f4fe88779f1
avelitj12 <avelitj12@gmail.com>
2025-04-19 16:16:53 -0400

    Spawn anemones at play area border

--- SCRIPTS/blast_attack.gd
+++ SCRIPTS/blast_attack.gd
@@ -6,7 +6,7 @@ var activated
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	activate()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
