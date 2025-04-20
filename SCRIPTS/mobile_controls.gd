616b8fb0452397b996ea142f507c35f3327d2e9e
avelitj12 <avelitj12@gmail.com>
2025-04-20 11:25:03 -0400

    New web export to fix mobile control buttons

--- SCRIPTS/mobile_controls.gd
+++ SCRIPTS/mobile_controls.gd
@@ -12,11 +12,20 @@ func _process(delta: float) -> void:
 
 
 func _on_blast_button_pressed() -> void:
-	Input.action_press("blast")
+	var blast = InputEventAction.new()
+	blast.action = "blast"
+	blast.pressed = true
+	Input.parse_input_event(blast)
 
 
 func _on_clam_button_button_down() -> void:
-	Input.action_press("tower")
+	var tower_press = InputEventAction.new()
+	tower_press.action = "tower"
+	tower_press.pressed = true
+	Input.parse_input_event(tower_press)
 
 func _on_clam_button_button_up() -> void:
-	Input.action_release("tower")
+	var tower_release = InputEventAction.new()
+	tower_release.action = "tower"
+	tower_release.pressed = false
+	Input.parse_input_event(tower_release)
